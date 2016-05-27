045163fc79330fdd71ee04607ebe1e308c0d8af5
Roman Lo <romam.nare@gmail.com>
2016-05-27 15:00:23 +0800

    Bug fix
    * Add path normalization to make sure the plugin works on different platforms.
    * Update unit test.
    * Update version.

--- test/test-path-setting.js
+++ test/test-path-setting.js
@@ -1,33 +1,34 @@
 var assert = require('chai').assert;
 var pathSetting = require('../lib/path-setting');
+var path = require('path');
 
 var cwd = process.cwd();
 
 describe('Path Setting', function() {
   describe('default path', function(){
     it('should return the default path', function () {
-      var expected = cwd + '/config/config.json';
+      var expected = path.normalize(cwd + '/config/config.json');
       assert.equal(pathSetting.buildConfigPath(), expected);
     });
   });
   describe('environment parameter test', function(){
     it('should return the path base on environment setting - NODE_ENV', function () {
-      var expected = cwd + '/config/config.test.json';
+      var expected = path.normalize(cwd + '/config/config.test.json');
       assert.equal(pathSetting.buildConfigPath('test'), expected);
     });
     
     it('should return the path base on environment setting - NODE_ENV + CONF_EXT_DIR', function () {
-      var expected = '/etc/myapp/config/config.test.json';
+      var expected = path.normalize('/etc/myapp/config/config.test.json');
       assert.equal(pathSetting.buildConfigPath('test', '/etc/myapp/config'), expected);
     });
 
     it('should return the path base on environment setting - NODE_ENV + CONF_EXT_DIR + CONF_EXT_NAME', function () {
-      var expected = '/etc/myapp/config/customize.other.json';
+      var expected = path.normalize('/etc/myapp/config/customize.other.json');
       assert.equal(pathSetting.buildConfigPath('other', '/etc/myapp/config', 'customize'), expected);
     });
 
     it('should return the path base on environment setting - NODE_ENV + CONF_EXT_DIR + CONF_EXT_NAME - relative path', function () {
-      var expected = cwd + '/base/conf2/customize.other.json';
+      var expected = path.normalize(cwd + '/base/conf2/customize.other.json');
       assert.equal(pathSetting.buildConfigPath('other', './base/conf/../conf2', 'customize'), expected);
     });
   });
